e223c9fe0ebc7c5083569376f7c38eca2884452a
cityking <154443603@qq.com>
2016-06-12 13:50:30 +0800

    通过XmlSerializer生成xml文件

--- MainActivity.java
+++ MainActivity.java
@@ -7,9 +7,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.xmlpull.v1.XmlSerializer;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Xml;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,41 +34,52 @@ public class MainActivity extends Activity {
 		
 	}
 	public void click(View v){
-		StringBuffer sb = new StringBuffer();
-		sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-		sb.append("<smss>");
-		for (Sms sms : smslist) {
-			sb.append("<sms>");
-			
-			sb.append("<adress>");
-			sb.append(sms.getAdress());
-			sb.append("</adress>");
-			
-			sb.append("<body>");
-			sb.append(sms.getBody());
-			sb.append("</body>");
-			
-			sb.append("<date>");
-			sb.append(sms.getDate());
-			sb.append("</date>");
-			
-			sb.append("</sms>");
-		}
-		sb.append("</smss>");
-		
 		try {
-			//记住添加权限
-			File file = new File(Environment.getExternalStorageDirectory().getPath(),"sms_bac.xml");
+			
+			XmlSerializer ser = Xml.newSerializer();
+			File file = new File(Environment.getExternalStorageDirectory(),"sms_bac2.xml");
 			FileOutputStream fos = new FileOutputStream(file);
-			fos.write(sb.toString().getBytes());
+			ser.setOutput(fos, "utf-8");
+			
+			ser.startDocument("utf-8", true);
+			
+			ser.startTag(null, "smss");
+			
+			for (Sms sms : smslist) {
+				ser.startTag(null, "sms");
+				
+				ser.startTag(null, "adress");
+				ser.text(sms.getAdress());
+				ser.endTag(null, "adress");
+				
+				ser.startTag(null, "body");
+				ser.text(sms.getBody());
+				ser.endTag(null, "body");
+				
+				ser.startTag(null, "date");
+				ser.text(sms.getDate());
+				ser.endTag(null, "date");
+				
+				ser.endTag(null, "sms");
+			}
+			
+			ser.endTag(null, "smss");
+			
+			ser.endDocument();
 			fos.close();
+			
+			
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
+
 
 
 }
