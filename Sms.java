c13d00a58d20fb747b9ea4af9aaf2cc08d5a791d
cityking <154443603@qq.com>
2016-06-12 11:46:22 +0800

    javaBean

---
+++ Sms.java
@@ -0,0 +1,25 @@
+package com.example.createxml;
+
+public class Sms {
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	private String adress;
+	private String body;
+	private String date;
+}
